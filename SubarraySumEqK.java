d9757661d8b2ab7b1c03563f9f52bb9e4f0a1f5f
mah0504 <maha.amhaouch@umontreal.ca>
2025-01-03 00:17:56 +0100

    nul mais better ig

--- SubarraySumEqK.java
+++ SubarraySumEqK.java
@@ -1,41 +1,47 @@
 public class SubarraySumEqK {
 
     int counter = 0;
-    int diff;
-    int pointer1;
+    int pointer1=0 ;
+    int pointer2= pointer1;
     public int subarraySum(int[] nums, int k) {
+        int diff=k;
 
+        
+        while ( pointer2 <nums.length){
 
+            // System.out.println("pointer atteint index  : "+pointer1);
 
-        // 1 1 1   pointer1 = 0   numsleng = 2
-        // 2 pointeurs initialement on increm le p2 si contrad on increm p1 
-        // et p2 revient a ce p1
+            diff = diff - nums[pointer2] ; //  nbr recherché 
+            System.out.println( "nbr soustrait "+nums[pointer2]);
+            System.out.println("la difference = "+ diff);
 
-        while ( pointer1 <nums.length-1){
-            System.out.println("pointer atteint index  : "+pointer1);
-
-            diff = k - nums[pointer1] ; // nbr recherché 
-
-            System.out.println("Difference atteinte : " + diff); //1 
+            // System.out.println("Difference atteinte : " + diff); //1 
 
             if ( diff == 0){
-                counter++;
+                counter++; 
                 System.out.println("Compteur atteint quand diff egale : "+counter);
-
-
-                System.out.println(" valeur suivante : "+ nums[pointer1+1]);
-
+                // pointer2 ++;
+                pointer1 =pointer2;
 
             } 
 
             // val suiv 
-            else if( nums[pointer1+1] >diff){ 
+            // nums[pointer2+1] >
+            else if( diff<0){ 
 
-                
+                pointer1++; 
+                pointer2=pointer1; 
                 break; // subarray invalide passer au suivant 
             }
 
-            pointer1 ++; 
+            if (pointer2 <nums.length-1){
+                pointer2 ++; 
+                System.out.println("nbr suivant "+nums[pointer2]);
+
+            } else {
+                break;
+            }
+
         }
 
         
@@ -46,8 +52,8 @@ public class SubarraySumEqK {
     public static void main(String[] args ){
 
         SubarraySumEqK arr = new SubarraySumEqK(); 
-        int[] tab0 = {1,1,1};
-        System.out.println(arr.subarraySum(tab0, 2));
+        int[] tab0 = {1,2};
+        System.out.println(arr.subarraySum(tab0, 3));
 
     }
 }
