3b3a72fb79ae117eaafc1ef48f7eaf82baf43281
mah0504 <maha.amhaouch@umontreal.ca>
2025-01-02 23:49:52 +0100

    idee ett

--- SubarraySumEqK.java
+++ SubarraySumEqK.java
@@ -5,31 +5,40 @@ public class SubarraySumEqK {
     int pointer1;
     public int subarraySum(int[] nums, int k) {
 
+
+
+        // 1 1 1   pointer1 = 0   numsleng = 2
+        // 2 pointeurs initialement on increm le p2 si contrad on increm p1 
+        // et p2 revient a ce p1
+
         while ( pointer1 <nums.length-1){
             System.out.println("pointer atteint index  : "+pointer1);
-            
+
             diff = k - nums[pointer1] ; // nbr recherché 
 
-            System.out.println("Difference atteinte : " + diff);
+            System.out.println("Difference atteinte : " + diff); //1 
 
             if ( diff == 0){
                 counter++;
-                System.out.println("Compteur atteint: "+counter);
+                System.out.println("Compteur atteint quand diff egale : "+counter);
+
+
+                System.out.println(" valeur suivante : "+ nums[pointer1+1]);
+
 
             } 
 
             // val suiv 
-            else if( nums[pointer1+1] >diff){
+            else if( nums[pointer1+1] >diff){ 
 
+                
                 break; // subarray invalide passer au suivant 
-            }
-            else {
-                diff -=( nums[pointer1+1] ); // decrem la val qu'on vient de trv 
             }
 
             pointer1 ++; 
         }
 
+        
         return counter ;
     }
 
